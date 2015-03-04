c4511714bcf6b72e08afb04e0e3df5ea998852e1
vishnu <svishnu5050@gmail.com>
2015-03-04 07:54:52 +0000

    unique validatoe error

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,8 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   default_scope -> { order(created_at: :desc) }
-  validates :user_id,presence: true
-  validates :title,presence: true,length: {maximum: 50}
-  validates :content,presence: true,length: {maximum: 140}
-  validates :slug,presence: true,unique: true
+  validates :user_id, presence: true
+  validates :title, presence: true, length: {maximum: 50}
+  validates :content, presence: true, length: {maximum: 140}
+  validates :slug, presence: true
 end
